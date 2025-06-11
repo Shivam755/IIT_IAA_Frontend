21bfdedad727dc6f3155121af8ba9045ca909f20
shivam755 <www.shivam567@gmail.com>
2025-06-11 23:44:11 +0530

    created a basic project and added dockerfile

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:24-alpine3.21 
+
+WORKDIR /frontend
+
+COPY package.json yarn.lock* package-lock.json* pnpm-lock.yaml* .npmrc* ./
+RUN \
+  if [ -f yarn.lock ]; then yarn --frozen-lockfile; \
+  elif [ -f package-lock.json ]; then npm ci; \
+  elif [ -f pnpm-lock.yaml ]; then corepack enable pnpm && pnpm i --frozen-lockfile; \
+  else echo "Lockfile not found." && exit 1; \
+  fi
+
+COPY . .
+
+RUN chmod +x entrypoint.sh
+
+EXPOSE 3000
+
+ENTRYPOINT [ "./entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Detect and run based on the lockfile
+if [ -f yarn.lock ]; then
+  echo "Detected yarn.lock, running: yarn dev"
+  yarn dev --hostname 0.0.0.0 --port 3000
+elif [ -f package-lock.json ]; then
+  echo "Detected package-lock.json, running: npm run dev"
+  npm run dev -- --hostname 0.0.0.0 --port 3000
+elif [ -f pnpm-lock.yaml ]; then
+  echo "Detected pnpm-lock.yaml, running: pnpm dev"
+  pnpm dev --hostname 0.0.0.0 --port 3000
+else
+  echo "No lockfile found, aborting."
+  exit 1
+fi
